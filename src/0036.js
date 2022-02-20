8968ee810d25a14ee948f35eee31b583d0ed36c1
Yenny <aldald73@naver.com>
2022-02-20 14:20:41 +0900

    36: SLL pop

--- src/0036.js
+++ src/0036.js
@@ -11,20 +11,34 @@ class SinglyLinkedList {
     this.tail = null;
     this.length = 0;
   }
-  push() {
-    var newNode = new Node(val);
-    if (!this.head) {
-      this.head = newNode;
-      this.tail = this.head;
-    } else {
+  push(val) {
+    let newNode = new Node(val);
+    if (this.head) {
       this.tail.next = newNode;
+      this.tail = newNode;
+    } else {
+      this.head = newNode;
       this.tail = newNode;
     }
     this.length++;
-
     return this;
   }
-  pop() {}
+  pop() {
+    if (this.length === 0) return undefined;
+    const oldTail = this.tail;
+    let newTail = this.head;
+    while (newTail.next !== this.tail && newTail !== this.tail) {
+      newTail = newTail.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return oldTail;
+  }
 }
 
 module.exports = SinglyLinkedList;
